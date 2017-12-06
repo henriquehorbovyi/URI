01cf83b64250328e1211643b56b32c09d70bffd1
Henry <carloshenry.wm@gmail.com>
2017-12-06 18:24:25 -0200

    Now this is right

--- Beginner/1158.py
+++ Beginner/1158.py
@@ -3,5 +3,12 @@ for i in range(0,N):
 	nums = input().split(" ")
 	X = int(nums[0])
 	Y = int(nums[1])
+	summ = 0
 	while True:
-		
+		if X%2 != 0:
+			summ += X
+			Y -= 1
+		X += 1
+		if(Y == 0):
+			break
+	print(summ)
